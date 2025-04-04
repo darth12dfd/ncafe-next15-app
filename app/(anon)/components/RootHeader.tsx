3471a68d054d5513f6aaf8076814d1bfe9a68f93
이규원 <woni011420@gmail.com>
2025-04-04 15:53:18 +0900

    admin/menus fetch 코드 추가가

--- app/(anon)/components/RootHeader.tsx
+++ app/(anon)/components/RootHeader.tsx
@@ -1,11 +1,11 @@
 "use client";
 import Link from "next/link";
-import styles from "./RootHeader.module.scss";
+import {header, a} from "./RootHeader.module.scss";
 
 const RootHeader = () => {
     return (
-        <header className = {styles["n-header"]}>
-            <h1>My Application</h1>
+        <header className={header}>
+            <h1 className={a}>My Application</h1>
             <nav>
                 <ul>
                     <li><Link href="/">Home</Link></li>
